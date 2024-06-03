0459df8bdef2b4a3100f2f97bcf7de2e27239d2d
jasa1aw <erakrut05@gmail.com>
2024-06-04 01:56:14 +0500

    first commit

---
+++ script/burgerMenu.js
@@ -0,0 +1,8 @@
+let menu = document.querySelector('.burgerMenuBackground');
+
+function openBurgerMenu(){
+    menu.classList.add('open-menu')
+}
+function closeBurgerMenu(){
+    menu.classList.remove('open-menu')
+}
